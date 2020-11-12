8c5502db353bc9d1d5cc00bdfa1c1c5724a12824
ko <ko>
2020-11-12 19:17:17 +0900

    백엔드와 연결.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from './Router';
-
+import axios from 'axios';
 const App = () => {
     // const [user , setUser] = useState(null);
+    const [call, setCall] = useState(null);
+    let URL =
+        'http://localhost:8080/SpringFileBoardPR/board/connect.do?name=testkim';
+    useEffect(() => {
+        const users = async () => {
+            try {
+                const response = await axios.get(URL);
+                setCall(response);
+            } catch (error) {
+                alert('에러입니다,');
+            }
+        };
+        users();
+    }, []);
+    console.log(call);
     return (
-        <AppRouter
-        // isLoggedIn={user ? true : false}
-        // onLogin={(loginUser) => {setUser(loginUser)}}
-        />
+        <>
+            <AppRouter
+            // isLoggedIn={user ? true : false}
+            // onLogin={(loginUser) => {setUser(loginUser)}}
+            />
+        </>
     );
 };
 
